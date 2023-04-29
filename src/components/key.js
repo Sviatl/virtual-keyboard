c1d8a5d10d1496935e46b2978cfe005a98720180
Sviatlana Yurusava <sv.yurusova@gmail.com>
2023-04-30 01:11:48 +0300

    feat: positions of keys

--- src/components/key.js
+++ src/components/key.js
@@ -1,9 +1,15 @@
 export default class Key {
   constructor(key, callback) {
-    this.key = key;
+    this.key = key.key;
     this.callback = callback;
     this.element = document.createElement('div');
     this.element.className = 'key';
+    if (key.grow) {
+      this.element.classList.add('key-grow');
+    }
+    if (key.grow2) {
+      this.element.classList.add('key-grow2');
+    }
     this.element.textContent = this.key;
     this.element.addEventListener('click', () => {
       this.callback(this.key);
